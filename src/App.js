661faeef82f2e393465d458e5c2378ce6541d69a
Tyler <tscizzle@gmail.com>
2017-03-17 13:37:20 -0400

    without npm cmaaahhhnn

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Girls Who Code Is The Gteatest</h2>
         </div>
         <p className="App-intro">
